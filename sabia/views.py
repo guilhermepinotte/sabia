285e0c3918ba6dd0299b96a28f5e9983707ba333
Guilherme Pinotte <guilhermepinotte@gmail.com>
2014-07-21 16:07:44 -0300

    Tela de cadastro de usuários funcionando

--- sabia/views.py
+++ sabia/views.py
@@ -11,21 +11,23 @@ def CadastroView(request):
 	return render(request,'sabia/cadastro.html')
 
 def CadastrarUsuario(request):
-	context = 'ok'
-	return render(request,'sabia/index.html',context)
-	# if request.method == 'POST':
-	# try:
-	# 	usuario = Usuario()
-	# 	usuario.email = request.POST['email']
-	# 	usuario.senha = request.POST['senha']
-	# 	usuario.nome = request.POST['nome']
-	# 	usuario.tipo = request.POST['tipo']
-	# 	usuario.ativo = False
-	# 	usuario.dataCadastro = timezone.now()
-	# 	usuario.save()
-	# 	context = 'ok'
-	# 	return render(request,'sabia/index.html',context)
-	# except KeyError:
-	# 	raise
-	# 	context = 'error'
-	# 	return render(request,'sabia/index.html',context)	
+	if request.method == 'POST':
+		try:
+			usuario = Usuario()
+			usuario.email = request.POST['email']
+			usuario.senha = request.POST['senha']
+			usuario.nome = request.POST['nome']
+			usuario.tipo = request.POST['tipo']
+			usuario.ativo = False
+			usuario.dataCadastro = timezone.now()	
+		except KeyError:
+			return render(request,'sabia/cadastro-msg.html',
+				{'error_message': "Erro no cadastro"})
+		else:
+			usuario.save()
+			return render(request,'sabia/cadastro-msg.html',
+				{'sucess_message': "Usuário cadastrado com sucesso"})
+				
+			# raise
+			# context = 'error'
+			# return render(request,'sabia/cadastro-msg.html',context)	
